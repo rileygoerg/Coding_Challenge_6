fa1db46850417b4e0ce15f0da93ef63ade85b0c2
rileygoerg <rgoerg@usf.edu>
2024-06-04 14:46:31 -0400

    Implement animation using setInterval.

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// U14123683
 let canvas = document.querySelector("#canvas"); // Initialize canvas
 let ctx = canvas.getContext("2d"); // Initialize 2d context
 let width = canvas.width;
@@ -19,13 +20,20 @@ class Ball { // Creating Ball class
         ctx.fill();
     }
     updateBall(){ // Collision Logic
-        this.x += this.dx;
-        this.y += this.dy;
+        this.x += this.dx; // Adds delta x to new x value
+        this.y += this.dy; // Adds delta y to new y value
         if(this.x + this.radius > canvas.width || this.x - this.radius <= 0){
-            this.dx = -this.dx;
+            this.dx = -this.dx; // Reverses delta x when x value reaches 0 or max canvas width
         }
         if(this.y + this.radius > canvas.height || this.y - this.radius <= 0){
-            this.dy = -this.dy;
+            this.dy = -this.dy; // Reverses delta y when y value reaches 0 or max canvas height
         }
     }
 }
+
+let ball = new Ball(200, 160, 20, 2, 2, "red"); //  New test ball data
+setInterval(() => { 
+    ctx.clearRect(0,0,canvas.width,canvas.height); // Clears old ball draw
+    ball.updateBall(); // Implements collision logic
+    ball.drawBall(); // Re-draws ball
+}, 10); // 
