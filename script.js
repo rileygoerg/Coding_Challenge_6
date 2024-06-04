c5564a5b1f5732905902e387bda9c91a3effab89
rileygoerg <rgoerg@usf.edu>
2024-06-04 14:01:29 -0400

    Implement draw method for Ball class.

--- script.js
+++ script.js
@@ -12,4 +12,10 @@ class Ball {
         this.dy = dy;
         this.color = color;
     }
+    drawCircle(){
+        ctx.fillStyle = this.color;
+        ctx.beginPath();
+        ctx.arc(this.x, this.y, this.radius, 0, Math.PI*2, false);
+        ctx.fill();
+    }
 }
